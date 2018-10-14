5768f6b7f4e10e47d6d0f74579e1733b2c864863
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2018-10-13 21:04:59 -0400

    Correction erreurs script.js

--- tdd.js
+++ tdd.js
@@ -1,107 +1,131 @@
+/*jslint browser:true, esnext:true*/
 /*
 TODO:Choix de langue
 TODO:Difficulté
 TODO:Ajouter des mots pas rapport
 */
-window.tdd = {
-	points:0,
-	but:10,
-	pause:10,	// Pause entre les mots en millisecondes
-	difficulte:2, // 1=facile plus de temps, 2=moyen 1 seconde par mot restant ou 3=Difficile moitie moins de temps pour répondre
+var tdd = {
+    points: 0,
+    but: 10,
+    pause: 10, // Pause entre les mots en millisecondes
+    difficulte: 2, // 1=facile plus de temps, 2=moyen 1 seconde par mot restant ou 3=Difficile moitie moins de temps pour répondre
 };
-function recupererGet() {
-	var resultat = {};
-	if (location.search == "") return resultat;
-	var ligne = location.search.substr(1);
-	var donnees = ligne.split(/&/g);
-	for (var i=0; i<donnees.length; i++) {
-		var d = donnees[i].split(/=/g);
-		if (d.length > 0) {
-			var nom = d.shift();
-			var val = d.join("=");
-			resultat[nom] = val;
-		}
-	}
-	return resultat;
+class Tdd {
+    static recupererGet() {
+        var resultat = {};
+        if (location.search === "") {
+            return resultat;
+        }
+        var ligne = location.search.substr(1);
+        var donnees = ligne.split(/&/g);
+        for (var i = 0; i < donnees.length; i += 1) {
+            var d = donnees[i].split(/=/g);
+            if (d.length > 0) {
+                var nom = d.shift();
+                var val = d.join("=");
+                resultat[nom] = val;
+            }
+        }
+        return resultat;
+    }
+    static affichage() {
+        var $resultat = $(document.createElement('div'))
+            .addClass('tdd');
+        $(document.createElement('div'))
+            .addClass('mot')
+            .text("??????")
+            .appendTo($resultat)
+            .bind('transitionend', evtDelaiFini);
+        $resultat.append(affichageChoix());
+        $resultat.append(affichageScore());
+        return $resultat;
+    }
+    static piger(array) {
+        var pos = Math.floor(Math.random() * array.length);
+        return array[pos];
+    }
+    static redemarrerChrono() {
+        var duration = tdd.but - tdd.points;
+        if (tdd.difficulte === 1) duration += 1;
+        else if (tdd.difficulte >= 3) duration *= 1.5 - ((tdd.difficulte - 1) / (tdd.difficulte));
+        tdd.regleTransition.MozTransitionDuration = duration + "s";
+        setTimeout(function () {
+            $('div.mot').text(tdd.mot).addClass("on");
+            $("div.score").removeClass("correct erreur delai");
+        }, tdd.pause);
+    }
+
+    static setEvts() {
+        this.evt = {
+            window: {
+                keydown: (e) => {
+                    for (var ch in tdd.choix) {
+                        var label = tdd.choix[ch].text().toUpperCase().charCodeAt(0);
+                        if (e.keyCode === label || e.keyCode === label + 32) {
+                            tdd.choix[ch].trigger("click");
+                        }
+                    }
+                }
+            }
+        };
+    }
+    static init() {
+        this.setEvts();
+    }
 }
-function evtTouche(e) {
-	for (var ch in tdd.choix) {
-		var label = tdd.choix[ch].text().toUpperCase().charCodeAt(0);
-		if (e.keyCode == label || e.keyCode == label+32) {
-			tdd.choix[ch].trigger("click");
-		}
-	}
-}
-function affichageTdd() {
-	var $resultat = $(document.createElement('div'))
-		.addClass('tdd')
-	;
-	$(document.createElement('div'))
-		.addClass('mot')
-		.text("??????")
-		.appendTo($resultat)
-		.bind('transitionend', evtDelaiFini)
-	;
-	$resultat.append(affichageChoix());
-	$resultat.append(affichageScore());
-	return $resultat
-}
+Tdd.init();
+
 function evtDelaiFini() {
-	if (tdd.points >= tdd.but) return;
-	if (tdd.difficulte > 2) tdd.points = 0;
-	else tdd.points -= 1;
-	// tdd.but++;
-	$(".score").addClass("delai")
-	majScore();
-	majMot();
+    if (tdd.points >= tdd.but) return;
+    if (tdd.difficulte > 2) tdd.points = 0;
+    else tdd.points -= 1;
+    // tdd.but += 1;
+    $(".score").addClass("delai")
+    majScore();
+    majMot();
 }
-function piger(array) {
-	var pos = Math.floor(Math.random()*array.length);
-	return array[pos];
-}
+
 function affichageChoix(etiquette, classe) {
-	$resultat = $(document.createElement("div")).addClass("choix")
-	for (var ch in tdd.choix) {
-		var $bouton = affichageBouton(tdd.choix[ch], ch);
-		$resultat.append($bouton);
-		tdd.choix[ch] = $bouton;
-	}
-	return $resultat;
+    $resultat = $(document.createElement("div")).addClass("choix")
+    for (var ch in tdd.choix) {
+        var $bouton = affichageBouton(tdd.choix[ch], ch);
+        $resultat.append($bouton);
+        tdd.choix[ch] = $bouton;
+    }
+    return $resultat;
 }
+
 function affichageBouton(etiquette, classe) {
-	var $resultat = $(document.createElement('div'))
-		.addClass(classe)
-		.html(etiquette)
-		.click(evtClic)
-	;
-	return $resultat;
+    var $resultat = $(document.createElement('div'))
+        .addClass(classe)
+        .html(etiquette)
+        .click(evtClic);
+    return $resultat;
 }
+
 function affichageScore() {
-	var $resultat = $('<div class="score"><span class="points">'+tdd.points+'</span>/<span class="but">'+tdd.but+'</span></div>');
-	return $resultat;
+    var $resultat = $('<div class="score"><span class="points">' + tdd.points + '</span>/<span class="but">' + tdd.but + '</span></div>');
+    return $resultat;
 }
+
 function evtClic(e) {
-	var $this = $(this);
-	var classe = $this.attr("class");
-	valider(classe);
-	majScore();
-	if (!verifierVictoire()) {
-		majMot();
-	}
+    var $this = $(this);
+    var classe = $this.attr("class");
+    valider(classe);
+    majScore();
+    if (!verifierVictoire()) {
+        majMot();
+    }
 }
+
 function verifierVictoire() {
-	if (tdd.points < tdd.but) return false;
-	$(".tdd").html('<div class="bravo">Bravo!</div>');
-	return true;
+    if (tdd.points < tdd.but) return false;
+    $(".tdd").html('<div class="bravo">Bravo!</div>');
+    return true;
 }
+
 function majScore() {
-	$("span.points").html(tdd.points);
-	$("span.but").html(tdd.but);
+    $("span.points").html(tdd.points);
+    $("span.but").html(tdd.but);
 }
-function redemarrerChrono() {
-	duration = tdd.but-tdd.points;
-	if (tdd.difficulte == 1) duration++;
-	else if (tdd.difficulte >= 3) duration *= 1.5-((tdd.difficulte-1)/(tdd.difficulte)) ;
-	tdd.regleTransition.MozTransitionDuration = duration+"s";
-	setTimeout(function(){$('div.mot').text(tdd.mot).addClass("on"); $("div.score").removeClass("correct erreur delai");}, tdd.pause);
-}
+
